423fcbbb6f83a5d40b853081f47b178af3284277
Leon Hwang <leon.hwang@linux.dev>
2025-06-02 16:46:10 +0800

    cmp: Add super-tab sourcing lazy.nvim doc
    Signed-off-by: Leon Hwang <leon.hwang@linux.dev>

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -8,6 +8,8 @@ return {
 		-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
 		"MunifTanjim/nui.nvim",
 
+		"hrsh7th/nvim-cmp",
+
 		-- OPTIONAL:
 		--   `nvim-notify` is only needed, if you want to use the notification view.
 		--   If not available, we use `mini` as the fallback
