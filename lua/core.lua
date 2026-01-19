be6bde6cf0ca819cd031d7bf88deed98184d1900
Leon Huang Fu <leon.huangfu@shopee.com>
2026-01-19 10:30:31 +0800

    c: Use tabs always
    Signed-off-by: Leon Huang Fu <leon.huangfu@shopee.com>

--- lua/core.lua
+++ lua/core.lua
@@ -216,9 +216,10 @@ local function set_autocmd()
 		group = "cfiles",
 		pattern = { "c" },
 		callback = function()
-			vim.opt_local.expandtab = true
 			vim.opt_local.tabstop = 8
 			vim.opt_local.shiftwidth = 8
+			vim.opt_local.expandtab = false -- use tabs
+			vim.opt_local.softtabstop = 0 -- stay as tabs
 		end,
 	})
 
