66bb00ddb9292a4409e4ae94e6608f8f1bd6fcc6
Leon Hwang <leon.hwang@linux.dev>
2026-06-25 10:57:11 +0800

    virtcolumn: Update columns to vim.g.colorcolumn
    Signed-off-by: Leon Hwang <leon.hwang@linux.dev>

--- lua/plugins/virtcolumn.lua
+++ lua/plugins/virtcolumn.lua
@@ -2,7 +2,7 @@ return {
 	"lukas-reineke/virt-column.nvim",
 	config = function()
 		require("virt-column").setup({
-			virtcolumn = "72,75,80,100",
+			virtcolumn = vim.g.colorcolumn,
 		})
 	end,
 }
